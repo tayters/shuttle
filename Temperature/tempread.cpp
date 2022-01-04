678daab359fa553657c4000f3dddbe538f3b8cb2
JR <jimmyrapson@gmail.com>
2022-01-05 12:39:21 +1300

    reading  temperature working

--- Temperature/tempread.cpp
+++ Temperature/tempread.cpp
@@ -1,37 +1,75 @@
+
+extern "C" {
+    #include <linux/i2c-dev.h>
+    #include <i2c/smbus.h>
+}
+
+#include <stdio.h>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+#include <stdint.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <asm/ioctl.h>
 #include <iostream>
 #include <errno.h>
-#include <wiringPiI2C.h>
-#include "Ezo_i2c.h"
+#include <unistd.h>
+//#include <wiringPiI2C.h>
+//#include <wiringPi.h>
+//#include "Ezo_i2c.h"
+
+#define	DEVICE_ID_0 0x64
+#define DEVICE_ID_1 0x65
 
 using std::cin;
 using std::cout;
 using std::endl;
+using namespace std;
 
-//
-Ezo_board TP = Ezo_board(0x65, "TP");
 
-#define	LED	8
 
 int main()
 {
-TP.send_read_cmd();
+  int fd;
+  int result;
+  int wbuf[1] = {'R'};
+  int data = 2;
+  int len = 16;
+  uint8_t buf[len];
 
-  /*
-  int fd, fd2, result;
 
-  fd = wiringPiI2CSetup(0x64);
-  cout << "Init result: "<< fd << endl;
+  if ((fd = open("/dev/i2c-1", O_RDWR)) < 0)
+  {
+    cout << strerror (errno) << endl;
+  }
 
 
-  fd2 = wiringPiI2CSetup(0x65);
-  cout << "2 result: "<< fd2 << endl;
-  result = wiringPiI2CWrite(fd, 'R');
+  if (ioctl (fd, I2C_SLAVE, 0x64) < 0)
+  {
+    cout << strerror (errno) << endl;
+  }
 
-      if(result == -1)
-      {
-         cout << "Error.  Errno is: " << errno << endl;
-      }
+  write(fd, wbuf, 1);
 
-*/
- return 0;
+  usleep(1000000);
+
+  read(fd,buf,len);
+
+  for(int i=0; i<len; i++)
+  {
+    cout << buf[i] ;
+  }
+
+  cout<<endl;
+
+  if(result == -1)
+  {
+    cout << "Error.  Errno is: " << errno << endl;
+  }
+
+
+  return 0;
 }
