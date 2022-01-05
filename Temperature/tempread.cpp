bda5c94ff288e073ee1edf2417352fa03d9e8fda
JR <jimmyrapson@gmail.com>
2022-01-05 15:34:51 +1300

    polling working

--- Temperature/tempread.cpp
+++ Temperature/tempread.cpp
@@ -9,7 +9,6 @@ extern "C" {
 #include <string>
 #include <stdlib.h>
 #include <stdint.h>
-#include <errno.h>
 #include <string.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
@@ -17,59 +16,64 @@ extern "C" {
 #include <iostream>
 #include <errno.h>
 #include <unistd.h>
-//#include <wiringPiI2C.h>
-//#include <wiringPi.h>
-//#include "Ezo_i2c.h"
 
-#define	DEVICE_ID_0 0x64
-#define DEVICE_ID_1 0x65
+
+#define	DEVICE_ADD_0 0x64
+#define DEVICE_ADD_1 0x65
+#define DEVICE_ADD_2 0x66
+
+#define READ_DELAY 700000
 
 using std::cin;
 using std::cout;
 using std::endl;
 using namespace std;
 
+const int READ_CMD[1] = {'R'};
 
+
+int i2c_device_init(int fd, int Addr)
+{
+  if ((fd = open("/dev/i2c-1", O_RDWR)) < 0)
+    cout << strerror (errno) << endl;
+
+  if (ioctl (fd, I2C_SLAVE, Addr) < 0)
+    cout << strerror (errno) << endl;
+
+  return fd;
+}
 
 int main()
 {
-  int fd;
+  int r;
   int result;
   int wbuf[1] = {'R'};
-  int data = 2;
   int len = 16;
   uint8_t buf[len];
+  int fd0, fd1, fd2;
 
+  fd0 = i2c_device_init(fd0, DEVICE_ADD_0);
+  fd1 = i2c_device_init(fd1, DEVICE_ADD_1);
+  fd2 = i2c_device_init(fd1, DEVICE_ADD_2);
 
-  if ((fd = open("/dev/i2c-1", O_RDWR)) < 0)
+  for(;;)
   {
-    cout << strerror (errno) << endl;
-  }
+    write(fd0, READ_CMD, 1);
+    write(fd1, READ_CMD, 1);
+    write(fd2, READ_CMD, 1);
 
+    usleep(READ_DELAY);
 
-  if (ioctl (fd, I2C_SLAVE, 0x64) < 0)
-  {
-    cout << strerror (errno) << endl;
-  }
+    read(fd0, buf, len);
+    cout << (char*)buf << endl;
 
-  write(fd, wbuf, 1);
+    read(fd1, buf, len);
+    cout << (char*)buf << endl;
 
-  usleep(1000000);
+    read(fd2, buf, len);
+    cout << (char*)buf << endl;
 
-  read(fd,buf,len);
-
-  for(int i=0; i<len; i++)
-  {
-    cout << buf[i] ;
-  }
-
-  cout<<endl;
-
-  if(result == -1)
-  {
-    cout << "Error.  Errno is: " << errno << endl;
-  }
-
+}
 
   return 0;
 }
