7d9f490f665eaa2f6e05d60e3ddabb07f45244db
JR <jimmyrapson@gmail.com>
2022-01-07 13:36:33 +1300

    init of combined main file shuttle.cpp

---
+++ Main/CMakeFiles/shuttle.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shuttle.dir/shuttle.cpp.o"
+  "shuttle"
+  "shuttle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shuttle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
